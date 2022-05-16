87af45bd10dae14594e5c32a7414a4f299a71907
Zart Qrom <zart_qrom@outlook.com>
2022-05-16 15:38:26 -0500

    Created games interface

--- retroGame.py
+++ retroGame.py
@@ -2,5 +2,24 @@
 ################################################
 # Author: Erick Hazel Rocha García
 # License: MIT
-# Description: Displays games avaialable to run
+# Description: Displays games available to run
 ################################################
+
+from tkinter import *
+
+class InterfazJuegos:
+
+    def __init__(self,):
+        #Inicializa la ventana
+        self.ventana = Tk()
+        self.ventana.geometry('1200x800')
+        self.ventana.title("Retro Games")
+
+        #Refresca la ventana
+        self.ventana.mainloop()
+
+def main():
+    interfaz = InterfazJuegos()
+    
+if __name__ == "__main__":
+    main()
