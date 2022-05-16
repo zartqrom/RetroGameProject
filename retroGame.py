c08a913ae62147b1f5f73a15c5974ae47bfab275
Zart Qrom <zart_qrom@outlook.com>
2022-05-16 15:00:21 -0500

    Added comments

--- retroGame.py
+++ retroGame.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python3
+################################################
+# Author: Erick Hazel Rocha García
+# License: MIT
+# Description: Displays games avaialable to run
+################################################
