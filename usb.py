a6f08262b4d891e12083e44dd85197e64cc1eb32
Zart Qrom <zart_qrom@outlook.com>
2022-05-20 18:48:57 -0500

    Added detection of each file in USB

--- usb.py
+++ usb.py
@@ -4,14 +4,25 @@
 # License: MIT
 # Description: Displays games available to run in Madfen
 ################################################
+import pathlib
 from time import sleep
+import shutil
 from psutil import disk_partitions
+
+destination = ""
 
 while True:
     sleep(1)
     for elem in disk_partitions():
         if 'removable' in elem.opts:
             print("USB encontrada", elem.device)
+            deviceDetected = str(elem.device)
+            contentUSB = pathlib.Path(deviceDetected)
+            for file in contentUSB.iterdir():
+                if file.is_file():
+                    completePathGame = deviceDetected+file
+                    shutil.copy(completePathGame, destination)
+            
             print("Pausar emulacion")
             print("Copiar ROMS")
             print("ROMs copiadas")
